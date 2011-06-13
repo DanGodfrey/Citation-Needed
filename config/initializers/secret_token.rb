af60e990dd79a47c2ddac60cbd8d43201b5b6c1d
dangodfrey <c.daniel.godfrey@gmail.com>
2011-06-13 00:06:49 -0700

    initial commit. Place holder site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citationneeded::Application.config.secret_token = 'a2a183905191b343a4f6c78fc17b1be3c9a46561b6dd52822c2926c40a482d0b5cf4207dc15cce89d1b23044ffeb896f81d6ec220e690e8b6c676b81adb2878a'
